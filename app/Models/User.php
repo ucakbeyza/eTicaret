9ea7eb609498d12dd244d3cb7d8796a50505e55c
Beyza UÇAK <144684543+ucakbeyza@users.noreply.github.com>
2025-08-22 15:55:24 +0300

    Removed: api_token

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'api_token',
+        
     ];
 
     /**
@@ -33,7 +33,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'api_token',
     ];
 
     /**
