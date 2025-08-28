385f95d8025af7c1dbab4171b0c1abdbcc8af04a
Beyza UÇAK <144684543+ucakbeyza@users.noreply.github.com>
2025-08-28 13:54:56 +0300

    add me endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
+//me endpoint to get the authenticated user details
+Route::middleware('auth:sanctum')->get('/me',[AuthController::class,'me']);
